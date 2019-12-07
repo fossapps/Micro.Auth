bee5dbb924dcd5e0f2067e71f2f124741f1af0de
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-12-07 22:49:07 +0700

    test(postman): cd into directory for postman file

--- Micro.Auth.UnitTest/ExternalTests/postman_tests.sh
+++ Micro.Auth.UnitTest/ExternalTests/postman_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-npx newman run https://www.getpostman.com/collections/38b3855cdbfaf9a33dc1 -e ./env.json && rm env.json
+npx newman run https://www.getpostman.com/collections/38b3855cdbfaf9a33dc1 -e ./env.json
