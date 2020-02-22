6740e900a6bf61af4ea49d10fcfd7051ae38e9c2
Nishchal Gautam <gautam.nishchal@gmail.com>
2020-02-22 14:11:01 +0700

    feat(cors): add cors functionality

---
+++ Micro.Auth.Api/StartupExtensions/Cors.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using Micro.Auth.Api.Configs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Micro.Auth.Api.StartupExtensions
+{
+    public static class Cors
+    {
+        public static void AddCorsPolicies(this IServiceCollection services, CorsConfig config)
+        {
+            services.AddCors(x =>
+            {
+                x.AddPolicy("development", builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+                x.AddPolicy("production", builder =>
+                {
+                    builder.WithOrigins(config.Origins.ToArray())
+                        .WithMethods(config.Headers.ToArray())
+                        .AllowAnyMethod();
+                    if (!config.AllowCredentials)
+                    {
+                        builder.DisallowCredentials();
+                        return;
+                    }
+                    builder.AllowCredentials();
+                });
+            });
+        }
+
+        public static void UseCorsPolicy(this IApplicationBuilder app, IWebHostEnvironment env, CorsConfig config)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors("development");
+                return;
+            }
+
+            app.UseCors(config.PolicyToUse);
+        }
+    }
+}
