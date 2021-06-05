e3a4b50f10b389812c289bca862a8b68dfe85d73
Nishchal Gautam <cyberhck@users.noreply.github.com>
2021-06-05 14:25:49 +0700

    chore(extends): fix naming

--- Micro.Auth.Api/GraphQL/Federation/ExtendsDirective.cs
+++ Micro.Auth.Api/GraphQL/Federation/ExtendsDirective.cs
@@ -4,7 +4,7 @@ namespace Micro.Auth.Api.GraphQL.Federation
 {
     public class ExtendsDirective : DirectiveGraphType
     {
-        public const string DirectiveName = "external";
+        public const string DirectiveName = "extends";
         public override bool? Introspectable => true;
 
         public ExtendsDirective() : base(DirectiveName, DirectiveLocation.Object, DirectiveLocation.Interface)
